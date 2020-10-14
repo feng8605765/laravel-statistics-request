6931e55cb27215024606788417ea0f2620ca8c4e
丁不四 <645074190@qq.com>
2020-10-14 16:14:09 +0800

    '我的第一个版本'

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Yqy\Test\Tools;
+
+$tools = new Tools();
+
+$tools->hello();
---
+++ helper/Yqy/Test/Tools.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Yqy\Test;
+
+class Tools {
+    public function __construct()
+    {
+
+    }
+
+    public function hello() {
+        echo "hello world";
+    }
+}
